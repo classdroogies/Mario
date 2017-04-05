3d441e80b105c9ab907548722b03f7404c6b693a
Classdroogies <seb.robin@hotmail.fr>
2017-04-05 08:23:18 +0200

    Ajoutez des fichiers projet.

---
+++ Mario/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            while (true)
+            {
+
+                Console.Beep(658, 125);
+                Console.Beep(1320, 500);
+                Console.Beep(990, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(1188, 250);
+                Console.Beep(1320, 125);
+                Console.Beep(1188, 125);
+                Console.Beep(1056, 250);
+                Console.Beep(990, 250);
+                Console.Beep(880, 500);
+                Console.Beep(880, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(1320, 500);
+                Console.Beep(1188, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(990, 750);
+                Console.Beep(1056, 250);
+                Console.Beep(1188, 500);
+                Console.Beep(1320, 500);
+                Console.Beep(1056, 500);
+                Console.Beep(880, 500);
+                Console.Beep(880, 500);
+                Thread.Sleep(250);
+                Console.Beep(1188, 500);
+                Console.Beep(1408, 250);
+                Console.Beep(1760, 500);
+                Console.Beep(1584, 250);
+                Console.Beep(1408, 250);
+                Console.Beep(1320, 750);
+                Console.Beep(1056, 250);
+                Console.Beep(1320, 500);
+                Console.Beep(1188, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(990, 500);
+                Console.Beep(990, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(1188, 500);
+                Console.Beep(1320, 500);
+                Console.Beep(1056, 500);
+                Console.Beep(880, 500);
+                Console.Beep(880, 500);
+                Thread.Sleep(500);
+                Console.Beep(1320, 500);
+                Console.Beep(990, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(1188, 250);
+                Console.Beep(1320, 125);
+                Console.Beep(1188, 125);
+                Console.Beep(1056, 250);
+                Console.Beep(990, 250);
+                Console.Beep(880, 500);
+                Console.Beep(880, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(1320, 500);
+                Console.Beep(1188, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(990, 750);
+                Console.Beep(1056, 250);
+                Console.Beep(1188, 500);
+                Console.Beep(1320, 500);
+                Console.Beep(1056, 500);
+                Console.Beep(880, 500);
+                Console.Beep(880, 500);
+                Thread.Sleep(250);
+                Console.Beep(1188, 500);
+                Console.Beep(1408, 250);
+                Console.Beep(1760, 500);
+                Console.Beep(1584, 250);
+                Console.Beep(1408, 250);
+                Console.Beep(1320, 750);
+                Console.Beep(1056, 250);
+                Console.Beep(1320, 500);
+                Console.Beep(1188, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(990, 500);
+                Console.Beep(990, 250);
+                Console.Beep(1056, 250);
+                Console.Beep(1188, 500);
+                Console.Beep(1320, 500);
+                Console.Beep(1056, 500);
+                Console.Beep(880, 500);
+                Console.Beep(880, 500);
+                Thread.Sleep(500);
+                Console.Beep(660, 1000);
+                Console.Beep(528, 1000);
+                Console.Beep(594, 1000);
+                Console.Beep(495, 1000);
+                Console.Beep(528, 1000);
+                Console.Beep(440, 1000);
+                Console.Beep(419, 1000);
+                Console.Beep(495, 1000);
+                Console.Beep(660, 1000);
+                Console.Beep(528, 1000);
+                Console.Beep(594, 1000);
+                Console.Beep(495, 1000);
+                Console.Beep(528, 500);
+                Console.Beep(660, 500);
+                Console.Beep(880, 1000);
+                Console.Beep(838, 2000);
+                Console.Beep(660, 1000);
+                Console.Beep(528, 1000);
+                Console.Beep(594, 1000);
+                Console.Beep(495, 1000);
+                Console.Beep(528, 1000);
+                Console.Beep(440, 1000);
+                Console.Beep(419, 1000);
+                Console.Beep(495, 1000);
+                Console.Beep(660, 1000);
+                Console.Beep(528, 1000);
+                Console.Beep(594, 1000);
+                Console.Beep(495, 1000);
+                Console.Beep(528, 500);
+                Console.Beep(660, 500);
+                Console.Beep(880, 1000);
+                Console.Beep(838, 2000);
+            }
+
+        }
+    }
+}
